e0a3fcf9ff3ae5a4b9124c9fc557c75d205f365d
migue <miguel.armas.developer@gmail.com>
2021-10-16 20:12:36 -0500

    login

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,6 +1,18 @@
 import React from "react";
-
+import FacebookLogin from 'react-facebook-login';
 const Home = () => {
-  return <div>home</div>
+    const responseFacebook = (response) => {
+        console.log(response);
+    }
+  return <div>
+      <FacebookLogin
+          appId="422517372598479"
+          autoLoad
+          callback={responseFacebook}
+          render={renderProps => (
+              <button onClick={renderProps.onClick}>This is my custom FB button</button>
+          )}
+      />
+  </div>
 }
 export default Home
